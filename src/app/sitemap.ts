a76a35454c90c20e9a22445ca81f051a91aebc15
DennisRono <dennisrkibet@gmail.com>
2024-07-22 21:56:39 +0300

    added sitemap to the portfolio for better SEO

---
+++ src/app/sitemap.ts
@@ -0,0 +1,54 @@
+import type { MetadataRoute } from 'next'
+
+import { getBaseUrl } from '@/lib/Helpers'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const response = await fetch(`${getBaseUrl}/api/blogs`, {
+    method: 'GET',
+    redirect: 'follow',
+    cache: 'no-store',
+  })
+
+  if (!response.ok) {
+    console.log('Failed to fetch blog data')
+  }
+
+  const blogs: any = await response.json()
+  const siteobj = {
+    url: `${getBaseUrl()}/`,
+    lastModified: new Date(),
+    changeFrequency: 'daily',
+    priority: 0.7,
+  }
+  const dynamicProdPages: any[] = Array.isArray(blogs)
+    ? blogs.map((item: any) => {
+        return {
+          ...siteobj,
+          url: `${getBaseUrl()}/product/${item.slug}`,
+          lastModified: item.updatedAt,
+        }
+      })
+    : []
+
+  return [
+    {
+      url: `${getBaseUrl()}/`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    {
+      url: `${getBaseUrl()}/resume`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: `${getBaseUrl()}/projects`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.7,
+    },
+    ...dynamicProdPages,
+  ]
+}
