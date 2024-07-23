6e13da582ad9d88398761318f1ebd0273eb2ab44
DennisRono <dennisrkibet@gmail.com>
2024-07-23 12:16:12 +0300

    completed mailing service for denniskibet resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -9,9 +9,6 @@ import ResumeForm from './ResumeForm'
 const page = () => {
   return (
     <div className="h-screen pt-4 overflow-hidden">
-      <h1 className="font-semibold text-center font-volte-rounded-bold text-8xl mt-4 text-[#2b2b2b] dark:text-[#e6e6e6]">
-        Hire Me
-      </h1>
       <Link
         href="/"
         className="flex items-center justify-start gap-2 group absolute top-4 left-4"
@@ -24,13 +21,25 @@ const page = () => {
           Dennis Kibet
         </span>
       </Link>
-      <div className="flex items-center justify-center h-full">
-        <div className="p-2 -mt-[20%]">
-          <div className="mb-4">
+      <div className="flex items-center justify-start h-full">
+        <div className="p-2">
+          <div className="flex items-center justify-center gap-4 mb-4">
+            <h1 className="text-center text-3xl xl:text-5xl 2xl:text-6xl font-bold text-[#2b2b2b] dark:text-[#e6e6e6] font-apercu">
+              <span
+                style={{
+                  backgroundClip: 'text',
+                  WebkitTextFillColor: 'transparent',
+                  backgroundImage: 'linear-gradient(90deg, #1d1335, #002259)',
+                }}
+                className="capitalize font-bold"
+              >
+                Hire Me
+              </span>
+            </h1>
             <Image
               src={Grad}
               alt="graduation icon image"
-              className="mx-auto h-[150px] w-[150px]"
+              className="mx-0 h-[80px] w-min"
             />
           </div>
           <div className="text-center flex flex-col items-center">
