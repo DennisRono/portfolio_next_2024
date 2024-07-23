af4c2e443db02225b4fca2e2840c2694394199be
DennisRono <dennisrkibet@gmail.com>
2024-07-23 12:20:23 +0300

    completed mailing service for denniskibet resume

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,32 +3,32 @@ import type { MetadataRoute } from 'next'
 import { getBaseUrl } from '@/lib/Helpers'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const response = await fetch(`${getBaseUrl()}/api/blogs`, {
-    method: 'GET',
-    redirect: 'follow',
-    cache: 'no-store',
-  })
+  // const response = await fetch(`${getBaseUrl()}/api/blogs`, {
+  //   method: 'GET',
+  //   redirect: 'follow',
+  //   cache: 'no-store',
+  // })
 
-  if (!response.ok) {
-    console.log('Failed to fetch blog data')
-  }
+  // if (!response.ok) {
+  //   console.log('Failed to fetch blog data')
+  // }
 
-  const blogs: any = await response.json()
-  const siteobj = {
-    url: `${getBaseUrl()}/`,
-    lastModified: new Date(),
-    changeFrequency: 'daily',
-    priority: 0.7,
-  }
-  const dynamicProdPages: any[] = Array.isArray(blogs)
-    ? blogs.map((item: any) => {
-        return {
-          ...siteobj,
-          url: `${getBaseUrl()}/product/${item.slug}`,
-          lastModified: item.updatedAt,
-        }
-      })
-    : []
+  // const blogs: any = await response.json()
+  // const siteobj = {
+  //   url: `${getBaseUrl()}/`,
+  //   lastModified: new Date(),
+  //   changeFrequency: 'daily',
+  //   priority: 0.7,
+  // }
+  // const dynamicProdPages: any[] = Array.isArray(blogs)
+  //   ? blogs.map((item: any) => {
+  //       return {
+  //         ...siteobj,
+  //         url: `${getBaseUrl()}/product/${item.slug}`,
+  //         lastModified: item.updatedAt,
+  //       }
+  //     })
+  //   : []
 
   return [
     {
@@ -49,6 +49,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'weekly',
       priority: 0.7,
     },
-    ...dynamicProdPages,
+    // ...dynamicProdPages,
   ]
 }
