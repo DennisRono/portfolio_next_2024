9b93fb7dfe001a263d7a39f1cd99e16a6b843fa7
Dennis Kibet <dennisrkibet@gmail.com>
2025-01-10 22:17:50 +0300

    fix: correct projects data length reference in Projects component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
     <>
       <Header />
       <div className="max-w-screen-2xl min-h-screen mx-auto px-2 xs:px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
-        <Projects no={projects.data.length} f={true} />
+        <Projects no={projects.length} f={true} />
       </div>
       <Footer />
     </>
